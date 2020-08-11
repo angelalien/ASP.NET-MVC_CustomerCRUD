7483df074e9da4042c0b96876ef54fc20457e959
angelalien <angelalien1221@gmail.com>
2020-08-11 23:47:50 +0800

    Add file

---
+++ WebApplication1/Controllers/CustomersController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            CustomersModel model = new CustomersModel();
+            model.CustomerList = model.GetCustomerList();
+            return View(model);
+        }
+
+        [HttpGet]//新增頁面
+        public ActionResult Create()
+        {
+            CustomersModel model = new CustomersModel();
+            return View(model);
+        }
+
+        [HttpPost]//執行新增
+        public ActionResult Create(CustomersModel model)
+        {
+            model.Add(model.Form);
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]//更新頁面
+        public ActionResult Details(string id)
+        {
+            CustomersModel model = new CustomersModel();
+            model.Form = model.GetCustomer(id);
+            return View(model);
+        }
+
+        [HttpPost]//執行更新
+        public ActionResult Update(CustomersModel model)
+        {
+            model.Update(model.Form);
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]//刪除頁面
+        public ActionResult Delete(string id)
+        {
+            CustomersModel model = new CustomersModel();
+            model.Form = model.GetCustomer(id);
+            return View(model);
+        }
+
+        [HttpPost]//執行刪除
+        public ActionResult Delete(CustomersModel model)
+        {
+            model.Delete(model.Form.CustomerID);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ WebApplication1/Models/CustomersModel.cs
@@ -0,0 +1,91 @@
+﻿using CustomerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    /// <summary>
+    /// 客戶資料模型
+    /// </summary>
+    public class CustomersModel
+    {
+        /// <summary>
+        /// 北風資料庫實體
+        /// </summary>
+        private NorthwindEntities db = new NorthwindEntities();
+
+        /// <summary>
+        /// 客戶清單
+        /// </summary>
+        public List<Customers> CustomerList { get;set;}
+
+        /// <summary>
+        /// 建構子
+        /// </summary>
+        public CustomersModel()
+        {
+            CustomerList = new List<Customers>();
+        }
+
+        /// <summary>
+        /// 取得所有客戶清單
+        /// </summary>
+        /// <returns></returns>
+        public List<Customers> GetCustomerList()
+        {
+            return db.Customers.ToList();
+        }
+
+        /// <summary>
+        /// 表單
+        /// </summary>
+        public Customers Form { get; set; }
+
+        /// <summary>
+        /// 新增客戶資料
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(Customers item)
+        {
+            db.Customers.Add(item);
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// 取得指定客戶資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Customers GetCustomer(string id)
+        {
+            return db.Customers.SingleOrDefault(q => q.CustomerID == id);
+        }
+
+        /// <summary>
+        /// 更新客戶資料
+        /// </summary>
+        /// <param name="item"></param>
+        public void Update(Customers item)
+        {
+            var entity = db.Customers.SingleOrDefault(q => q.CustomerID == item.CustomerID);
+            entity.CompanyName = item.CompanyName;
+            entity.ContactName = item.ContactName;
+            entity.Phone = item.Phone;
+            entity.Country = item.Country;
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// 刪除指定客戶資料
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(string id)
+        {
+            var entity = db.Customers.SingleOrDefault(q => q.CustomerID == id);
+            db.Customers.Remove(entity);
+            db.SaveChanges();
+        }
+    }
+}
